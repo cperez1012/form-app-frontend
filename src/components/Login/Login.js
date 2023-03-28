14cf82045713e136ff99cb821f12d7949eace3ad
cperez1012 <50646333+cperez1012@users.noreply.github.com>
2023-03-28 11:54:35 -0400

    Added Home, Login, MainHeader, Button, and Card components and css files. Added logic all but MainHeader component

---
+++ src/components/Login/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
+
+const Login = (props) => {
+  return (
+    <>
+    </>
+  )
+}
+
+
+
+export default Login;
